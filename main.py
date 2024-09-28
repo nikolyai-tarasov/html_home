50c3ff62cb4bd675a4cf714a85336e64e0a0d2c5
Nikolay Tarasov <kolya.tarasov333@gmail.com>
2024-09-28 13:06:45 +0300

    option-1

---
+++ main.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        page_address = query_components.get('page')
+        page_content = self.__get_index()
+        if page_address:
+            page_content = self.__get_blog_article(page_address[0])
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
